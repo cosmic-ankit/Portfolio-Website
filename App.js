60d8a17d04ea4595781a8cdf5ae7b1d2b334e285
Ankit Jain <76097952+cosmic-ankit@users.noreply.github.com>
2023-06-25 23:51:53 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './Navbar';
+import HeroSection from './HeroSection';
+import AboutSection from './AboutSection';
+import ContactSection from './ContactSection';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <ContactSection />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
